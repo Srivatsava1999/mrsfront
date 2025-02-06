53b3aedb2be63556f7defe1731ba7e2e5a7ea086
Srivatsava1999 <srivatsava.dhanala@gmail.com>
2025-02-05 23:21:11 -0800

    final commit of the day

--- movie_reservation/src/App.js
+++ movie_reservation/src/App.js
@@ -1,9 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import MovieForm from "./components/MovieForm";
-import TheatreForm from "./components/TheatreForm";
-import NavbarComponent from "./components/NavbarComponent";
 import AddMovie from './pages/addmovie';
 import HomePage from './pages/HomePage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
--- movie_reservation/src/components/NavbarComponent.js
+++ movie_reservation/src/components/NavbarComponent.js
@@ -61,7 +61,7 @@ const NavbarComponent = () =>{
                     </Link>
                 </li>
                 <li className="nav-item">
-                    <Link to="add-theatre" className="nav-link">
+                    <Link to="/add-theatre" className="nav-link">
                         <svg 
                         xmlns="http://www.w3.org/2000/svg" 
                         viewBox="0 0 512 512">
