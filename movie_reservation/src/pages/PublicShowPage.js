54a3ea99b136039751f143e3eb039d46fedaaa4f
Srivatsava1999 <srivatsava.dhanala@gmail.com>
2025-03-04 22:03:27 -0800

    Final commit of the day.

--- movie_reservation/src/pages/PublicShowPage.js
+++ movie_reservation/src/pages/PublicShowPage.js
@@ -1,37 +1,18 @@
 import { useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
 import DropdownComponent from "../components/DropdownComponent";
-import EnterpriseNavbarComponent from "../components/EnterpriseNavbarComponent";
+import NavbarComponent from "../components/NavbarComponent";
 import DisplayScreenComponent from "../components/DisplayScreenComponent";
 import "./PublicShowPage.css";
 
-const PublicShowPage=()=>{
-  const {theatreId}=useParams()
-  const [shows,setShows]=useState([]);
-  const [movies, setMovies]=useState([]);
+const PublicShowPage=({shows})=>{
   const [selectedShow, setSelectedShow]=useState("")
-
-  useEffect(() => {
-      fetch(`${process.env.REACT_APP_BASE_API_URL}/theatresall/${theatreId}/showall/`,{
-        method: "GET",
-        headers:{
-            "Content-Type": "application/json"
-        }
-    }).then(response => response.json()).then(data=>{
-            const showArray = Object.values(data);  
-      setShows(showArray)
-    }).catch(error=>console.error("Error fetching Shows", error));
-    }, []);
-    useEffect(()=>{
-      fetch(`${process.env.REACT_APP_BASE_API_URL}/moviesall/`).then(respone=>respone.json()).then(data=>setMovies(data))
-      .catch(error=>console.error("Error fetching Shows", error));
-    });
   return (
       <section className="app-container">
         <style>
             @import url('https://fonts.googleapis.com/css2?family=Young+Serif&display=swap');
           </style>
-        <EnterpriseNavbarComponent/>
+        <NavbarComponent/>
         <main className="select-main">
           <section className="selection">
           <h2>Select Show</h2>
--- movie_reservation/src/pages/PublicTheatrePage.js
+++ movie_reservation/src/pages/PublicTheatrePage.js
@@ -1,13 +1,14 @@
 import { useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import DropdownComponent from "../components/DropdownComponent";
-import EnterpriseNavbarComponent from "../components/EnterpriseNavbarComponent";
+import NavbarComponent from "../components/NavbarComponent";
 import "./PublicTheatrePage.css";
 
 const PublicTheatrePage=()=>{
     const navigate=useNavigate()
     const [theatres,setTheatres]=useState([]);
     const [selectedTheatre, setSelectedTheatre]=useState("")
+    const [shows,setShows]=useState([]);
 
     useEffect(() => {
         fetch(`${process.env.REACT_APP_BASE_API_URL}/theatresall/`,{
@@ -20,12 +21,23 @@ const PublicTheatrePage=()=>{
             setTheatres(theatreArray);})
         .catch(error=>console.error("Error fetching theatres", error));
       }, []);
+    const HandleSubmit=()=>{
+        fetch(`${process.env.REACT_APP_BASE_API_URL}/theatresall/${selectedTheatre}/showall/`,{
+            method: "GET",
+            headers:{
+            "Content-Type": "application/json"
+            }
+        }).then(response => response.json()).then(data=>{
+            const showArray = Object.values(data);  
+            setShows(showArray)
+        }).catch(error=>console.error("Error fetching Shows", error));
+    };
     return (
         <section className="app-container">
             <style>
             @import url('https://fonts.googleapis.com/css2?family=Young+Serif&display=swap');
           </style>
-          <EnterpriseNavbarComponent/>
+          <NavbarComponent/>
           <main className="select-main">
             <section className="selection">
             <h2>Select Theatre</h2>
@@ -36,9 +48,9 @@ const PublicTheatrePage=()=>{
             onChange={(value) => setSelectedTheatre(value)}
             />
             </section>
-            <section className="buttons">
+            <section className="buttons" >
             <button
-                onClick={() => navigate(`/select-screen/${selectedTheatre}/publicscreen/`)}
+                onClick={HandleSubmit}
                 disabled={!selectedTheatre}
             >
                 View Shows
